4c797947d2589da4bdaa46a357cc5060f49fc74c
clivekiptoo <ckiptoo338@gmail.com>
2026-05-14 14:42:35 +0300

    add the search functionality of the page

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ window.onload = function () {
 const searchInput = document.getElementById("searchInput");
 const cards = document.querySelectorAll(".hustle-card");
 
+if (searchInput) {
+    searchInput.addEventListener("keyup", function () {
+        let searchValue = searchInput.value.toLowerCase();
+
+        cards.forEach(function(card) {
+            let cardText = card.textContent.toLocaleLowerCase();
+
+            if(cardText.includes(searchValue)){
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+}
