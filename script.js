0c9d75df8a5520faa72d8427ee53e4e6f635bb98
clivekiptoo <ckiptoo338@gmail.com>
2026-05-14 14:16:47 +0300

    Add search functionality for hustle-card

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 window.onload = function () {
     alert("Welcome to Hustle Marketplace");
 };
+
+
+
+// Search functionality
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".hustle-card");
+
